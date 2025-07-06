3177ec279cbdd1d691f2026dc29736658e047692
rcanifru <canifruelizalder@outlook.com>
2025-07-06 15:11:21 -0400

    primer commit

---
+++ react-api-pokemon-Lunes07/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PokemonFetcher from './PokemonFetcher';
+function App(){
+  return (
+    <>
+    <h1> Conoce a tu Pokemones</h1>
+    <PokemonFetcher />
+    </>
+  );
+}
+
+export default App;
